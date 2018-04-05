7f24e4691e3b31097332adf966e84369a873a827
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-04-05 14:09:03 +0200

    Se genera en recuadro negro de 320x480

---
+++ js/game.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function() {
+    // Set up an instance of the Quintus engine and include
+    // the Sprites, Scenes, Input and 2D module. The 2D module
+    // includes the `TileLayer` class as well as the `2d` componet.
+    var Q = window.Q = Quintus()
+        .include("Sprites, Scenes, Input, Touch, UI")
+        // Maximize this game to whatever the size of the browser is
+        .setup({
+            width: 480,
+            height: 320
+        })
+        // And turn on default input controls and touch input (for UI)
+        .controls().touch();
+});
