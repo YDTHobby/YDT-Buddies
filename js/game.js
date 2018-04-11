f7dd6582f7f9dba571104ff91d4c51ee4935c548
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-04-11 14:52:33 +0200

    Punto 3.2 realizado

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
     // Set up an instance of the Quintus engine and include
     // the Sprites, Scenes, Input and 2D module. The 2D module
     // includes the `TileLayer` class as well as the `2d` componet.
-    var Q = window.Q = Quintus()
-        .include("Sprites, Scenes, Input, Touch, UI")
+    var Q = Quintus()
+        .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX')
         // Maximize this game to whatever the size of the browser is
         .setup({
             width: 320,
@@ -11,4 +11,14 @@ window.addEventListener("load", function() {
         })
         // And turn on default input controls and touch input (for UI)
         .controls().touch();
+
+    Q.scene('level1', function(stage) {
+        Q.stageTMX('level.tmx', stage);
+        stage.add("viewport");
+        stage.viewport.offsetX = 150;
+        stage.viewport.offsetY = 380;
+    });
+    Q.loadTMX('level.tmx', function() {
+        Q.stageScene('level1');
+    });
 });
