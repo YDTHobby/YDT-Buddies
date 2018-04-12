4b605fec035450bb1dcf50e87716b6726eee6281
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-04-12 15:40:02 +0200

    12/04/2018

--- js/game.js
+++ js/game.js
@@ -20,14 +20,26 @@ window.addEventListener('load', function() {
                 y: 380
             });
             this.add('2d, platformerControls');
+        },
+
+        step: function(dt){
+            if(this.p.y > 580){
+                this.p.x = 150;
+                this.p.y = 380;
+            }
         }
+
     });
 
     Q.scene('level1', function(stage) {
         Q.stageTMX('level.tmx', stage);
 
         var mario = stage.insert(new Q.Mario());
-        stage.add("viewport").follow(mario);
+        stage.add("viewport").follow(mario, {x: true, y: true},{minX: -100, maxX: 256*16, minY: 125, maxY: 32*16});
+
+        stage.viewport.offsetX = -10;
+        stage.viewport.offsetY = 168;
+
     });
 
     Q.loadTMX('level.tmx, mario_small.png, mario_small.json', function() {
