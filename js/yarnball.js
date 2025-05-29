7222744c282772d7ca2e5bef70cbfd6effa16ef5
YDTHobby <ydt.hobby@gmail.com>
2025-05-29 08:04:13 -0600

    Update and rename coin.js to yarnball.js

--- js/yarnball.js
+++ js/yarnball.js
@@ -1,19 +1,19 @@
-function loadCoin(Q) {
+function loadYarnball(Q) {
     
-    Q.animations('coin animation', {
+    Q.animations('Yarnball animation', {
         'live': { frames: [0, 1, 2], rate: 1 / 3 }
     });
     /**
      * Clase que representa a una moneda.
      */
-    Q.Sprite.extend('Coin', {
+    Q.Sprite.extend('Yarnball', {
         init: function(p) {
             this._super(p, {
-                sprite: 'coin animation',
+                sprite: 'Yarnball animation',
                 /**
                  * Sprite de la moneda.
                  */
-                sheet: 'coin',
+                sheet: 'Yarnball',
                 /**
                  * Activamos el sensor de la moneda.
                  */
@@ -33,8 +33,8 @@ function loadCoin(Q) {
             this.animate({ y: this.p.y - 50 }, 0.3, { callback: get });
             if (!this.p.get) {
                 this.p.get = true;
-                Q.state.inc('coins', 1);
-                Q.audio.play('coin.mp3');
+                Q.state.inc('Yarnballs', 1);
+                Q.audio.play('Yarnball.mp3');
             }
         },
 
